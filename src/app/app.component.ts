7f61a19d9440795ab1390cedf225276c826919ed
darya-startsava <daryastartsava@gmail.com>
2024-04-16 19:04:19 +0200

    feat: add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { GarageComponent } from './garage/garage.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { WinnersComponent } from './winners/winners.component';
+
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'garage' },
+  { path: 'garage', component: GarageComponent },
+  { path: 'winners', component: WinnersComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
