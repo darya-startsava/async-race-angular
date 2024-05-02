20a0bfcc77a6bc1a1a70a0aa316be38bdbb790fe
darya-startsava <daryastartsava@gmail.com>
2024-05-03 00:26:19 +0200

    feat: add not found page

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.scss'
 })
